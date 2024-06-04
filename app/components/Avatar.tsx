fc5723eb3cc31bc2c15102da6f9b2b04adbce92d
shrria <58872002+shrria@users.noreply.github.com>
2024-06-04 17:18:21 +0700

    ✨ Navbar UI

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+
+const Avatar = () => {
+    return (
+        <Image
+            className="rounded-full"
+            alt="Avatar"
+            height={30}
+            width={30}
+            src="/images/avatar.jpg"
+        />
+    );
+}
+
+export default Avatar;
---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { BiSearch } from "react-icons/bi";
+
+const Search = () => {
+    return (
+        <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
+            <div className="flex flex-row items-center justify-between">
+                <div className="px-6 text-sm font-semibold">
+                    Anywhere
+                </div>
+                <div className="hidden sm:block px-6 border-x-[1px] flex-1 text-sm text-center font-semibold">
+                    Any Week
+                </div>
+                <div className="flex flex-row gap-3 pl-6 pr-2 items-center text-sm text-gray-600">
+                    <div className="hidden sm:block">Add Guests</div>
+                    <div className="p-2 bg-rose-500 rounded-full text-white">
+                        <BiSearch size={18} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Search;
