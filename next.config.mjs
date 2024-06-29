930203ff20a2a327c2d4120179e5ecb3662d4329
shrria <58872002+shrria@users.noreply.github.com>
2024-06-29 15:57:38 +0700

    🔧 Config images allowed by patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
