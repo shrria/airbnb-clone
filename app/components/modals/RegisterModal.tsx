6d823c2bab7515de9b22e451bf9b5811bdcdcecc
shrria <58872002+shrria@users.noreply.github.com>
2024-06-29 23:34:25 +0700

    🚸 Update after registered

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -43,9 +43,11 @@ const RegisterModal = () => {
 
         axios.post("/api/register", data)
             .then(() => {
+                toast.success("Success!");
                 registerModal.onClose();
+                loginModal.onOpen();
             })
-            .catch((error) => {
+            .catch(() => {
                 toast.error("Something went wrong.");
             }).finally(() => {
                 setIsLoading(false);
