e282f9429402d901c96b2f26ceeecc4f3f0bfccb
shrria <58872002+shrria@users.noreply.github.com>
2024-06-29 16:47:14 +0700

    🔨 Prepare desc, price modals

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -15,6 +15,7 @@ import CategoryInput from "@/app/components/input/CategoryInput";
 import CountrySelect from "@/app/components/input/CountrySelect";
 import Counter from "@/app/components/input/Counter";
 import ImageUpload from "@/app/components/input/ImageUpload";
+import Input from "@/app/components/input/Input";
 
 
 enum STEPS {
@@ -30,6 +31,7 @@ const RentModal = () => {
     const rentModal = useRentModal();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
 
     const {
         register,
@@ -184,6 +186,56 @@ const RentModal = () => {
             </div>
         )
     }
+
+    if (step === STEPS.DESCRIPTION) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="How would you describe your place?"
+                    subtitle="Short and sweet works best!"
+                />
+                <Input
+                    id="title"
+                    label="Title"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+                <hr />
+                <Input
+                    id="description"
+                    label="Description"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        );
+    }
+
+    if (step === STEPS.PRICE) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Now, set your price"
+                    subtitle="How much do you charge per night?"
+                />
+                <Input
+                    id="price"
+                    label="Price"
+                    formatPrice
+                    type="number"
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        );
+    }
+
 
     return (
         <Modal
